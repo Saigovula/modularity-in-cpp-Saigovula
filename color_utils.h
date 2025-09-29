29ece446d0c737f225ef4bc66c5d827d5932d702
Saigovula <129597706+Saigovula@users.noreply.github.com>
2025-09-29 10:00:19 +0530

    Create color_utils.h

---
+++ color_utils.h
@@ -0,0 +1,9 @@
+#ifndef COLOR_UTILS_H
+#define COLOR_UTILS_H
+
+#include "color_pair.h"
+
+ColorPair getColorFromPairNumber(int pairNumber);
+int getPairNumberFromColor(enum MajorColor major, enum MinorColor minor);
+
+#endif
