17dc4d09c2303bd0e3090444e98b94aaaa816901
Saigovula <129597706+Saigovula@users.noreply.github.com>
2025-09-29 10:59:15 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
 #include "color_utils.h"
-#include "color_code_manual.h"
+#include "color_code_manual_tests.h"
+#include "number2color_color2number_tests.h"
 #include <stdio.h>
 #include <assert.h>
-
-void testNumberToPair(int pairNumber, enum MajorColor expMajor, enum MinorColor expMinor) {
-    ColorPair cp = getColorFromPairNumber(pairNumber);
-    printf("Got pair %s\n", colorPairToString(&cp));
-    assert(getMajor(&cp) == expMajor && getMinor(&cp) == expMinor);
-}
-
-void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expected) {
-    int pairNum = getPairNumberFromColor(major, minor);
-    printf("Got pair number %d\n", pairNum);
-    assert(pairNum == expected);
-}
 
 int main() 
 {
