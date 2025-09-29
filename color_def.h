16594dcd001aa0e5959f2b39a92c2d9db9c79a74
Saigovula <129597706+Saigovula@users.noreply.github.com>
2025-09-29 09:58:49 +0530

    Update and rename color_code.h to color_def.h

---
+++ color_def.h
@@ -0,0 +1,12 @@
+#ifndef COLOR_DEFS_H
+#define COLOR_DEFS_H
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern const int numberOfMajorColors;
+extern const int numberOfMinorColors;
+
+#endif
