18f12c6c0d804571339bbcd338b2157ca3b132d5
Saigovula <129597706+Saigovula@users.noreply.github.com>
2025-09-29 09:59:21 +0530

    Create color_pair.h

---
+++ color_pair.h
@@ -0,0 +1,16 @@
+#ifndef COLOR_PAIR_H
+#define COLOR_PAIR_H
+
+#include "color_defs.h"
+
+typedef struct {
+    enum MajorColor major;
+    enum MinorColor minor;
+} ColorPair;
+
+ColorPair makeColorPair(enum MajorColor major, enum MinorColor minor);
+enum MajorColor getMajor(ColorPair* pair);
+enum MinorColor getMinor(ColorPair* pair);
+const char* colorPairToString(ColorPair* pair);
+
+#endif
